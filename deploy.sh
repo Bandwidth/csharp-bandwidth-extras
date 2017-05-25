a96502be884f7b83d4a8bef9cec0580e61f9d29e
avbel <belchikovs@gmail.com>
2017-05-25 11:32:26 +0300

    CHabged deploy script to use docfx via docker

--- deploy.sh
+++ deploy.sh
@@ -5,16 +5,10 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 function docGen {
-	npm run docs
-	DOCS_DIR=`find ./out/@bandwidth -name index.html`
-	DOCS_DIR=`dirname "$DOCS_DIR"`
-  mv $DOCS_DIR/* ./out
-	rm -rf ./out/@bandwidth
+	docker run -t -t --rm -v .:/src -w /src tsgkadot/docker-docfx docfx metadata && docfx build
 }
-NODE_VERSION=`node --version`
-NODE_VERSION=${NODE_VERSION:1:1}
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" -o "$NODE_VERSION" == "7" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
     exit 0
 fi
@@ -24,21 +18,21 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-# Clone the existing gh-pages for this repo into out/
+# Clone the existing gh-pages for this repo into .doc/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO out
-cd out
+git clone $REPO .doc
+cd .doc
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
 # Clean out existing contents
-rm -rf out/**/* || exit 0
+rm -rf .doc/**/* || exit 0
 
 # Run our compile script
 docGen
 
 # Now let's go have some fun with the cloned repo
-cd out
+cd .doc
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
